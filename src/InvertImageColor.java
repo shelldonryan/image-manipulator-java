5a375931f2f1f8791749b0ab8e8739fafd311248
shelldonryan <shelldon.boy15@gmail.com>
2024-08-21 19:48:40 -0300

    Create an algorithm to invert the color of the image

--- src/InvertImageColor.java
+++ src/InvertImageColor.java
@@ -3,9 +3,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.sql.Array;
 
-public class TransformColorInImageToBlack {
+public class InvertImageColor {
     public static void main(String[] args) throws IOException {
         String pathImage = "D:\\College-ComputerScience\\4º Período\\Image-processing-and-computer-vision\\image-manipulation\\src\\assets\\image.png";
         BufferedImage image = ImageIO.read(new File(pathImage));
@@ -14,22 +13,23 @@ public class TransformColorInImageToBlack {
         int height = image.getHeight();
 
         for (int column = 0; column < width; column++) {
-            for (int line = 0; line < height; line ++) {
-                int rgb = image.getRGB(column, line);
+            for (int row = 0; row < height; row++) {
+                int rgb = image.getRGB(column, row);
 
                 Color color = new Color(rgb);
 
-                int r = color.getRed();
-                int b = color.getBlue();
-                int g = color.getGreen();
+                int red = color.getRed();
+                int green = color.getGreen();
+                int blue = color.getBlue();
 
-                if (r >= 100 && g <= 75 && b <= 75) {
-                    image.setRGB(column, line, Color.BLACK.getRGB());
-                }
+                int redInvert = 255 - red;
+                int greenInvert = 255 - green;
+                int blueInvert = 255 - blue;
+
+                image.setRGB(column, row, new Color(redInvert, greenInvert, blueInvert).getRGB());
             }
         }
 
-        ImageIO.write(image, "png", new File("src\\assets\\modify_color_image.png"));
-
+        ImageIO.write(image, "png", new File("src\\assets\\invert_color_image.png"));
     }
 }
